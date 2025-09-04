9ed879494e4b34f26f07a9cfa8c6ac8c9eb0ddc3
Lexiao Lin <2623459403@qq.com>
2025-09-04 13:31:05 +0800

    update ACAM

--- src/string/ACAM.cpp
+++ src/string/ACAM.cpp
@@ -1,34 +1,29 @@
-struct ACAM {
-    int ch[N][26], cnt, fail[N], vis[N];
-    queue <int> q;
-    void insert(char *s, int n, int id) {
-        int x = 0;
-        for (int i = 1; i <= n; i++) {
-            int nw = s[i] - 'a';
-            if (!ch[x][nw]) ch[x][nw] = ++cnt;
-            x = ch[x][nw];
-        }
-        vis[x]++;
+// 记得修改字符集大小和字符种类
+const int SIGMA = 26;
+const char BASE = 'a';
+int ch[N][SIGMA],fail[N],cnt[N],tot=1;
+void ins(const string& s){
+    int u=1;
+    for(int i=0;i<s.size();i++){
+        int c=s[i]-BASE;
+        if(!ch[u][c])ch[u][c]=++tot;
+        u=ch[u][c];
     }
-    void build() {
-        for (int i = 0; i < 26; i++) {
-            if (ch[0][i]) q.push(ch[0][i]);
-        }
-        while (!q.empty()) {
-            int x = q.front(); q.pop();
-            for (int i = 0; i < 26; i++) {
-                if (ch[x][i]) {
-                    fail[ch[x][i]] = ch[fail[x]][i];
-                    q.push(ch[x][i]);
-                } else ch[x][i] = ch[fail[x]][i];
+    cnt[u]++;
+}
+void get_fail(){
+    for(int i=0;i<SIGMA;i++)ch[0][i]=1;
+    fail[1]=0;
+    queue<int> q;q.push(1);
+    while(q.size()){
+        int u=q.front();q.pop();
+        for(int c=0;c<SIGMA;c++){
+            int v=ch[u][c];
+            if(!v)ch[u][c]=ch[fail[u]][c];
+            else{
+                fail[v]=ch[fail[u]][c];
+                q.push(v);
             }
         }
     }
-    void match(char *s, int n) {
-        int x = 0;
-        for (int i = 1; i <= n; i++) {
-            int nw = s[i] - 'a';
-            x = ch[x][nw];
-        }
-    }
-} AC;
+}

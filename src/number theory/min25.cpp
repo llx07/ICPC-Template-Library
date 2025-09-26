8ed42f9aa60ad89fdef3618fbec10ec67ba41584
Lexiao Lin <2623459403@qq.com>
2025-09-26 18:06:38 +0800

    update min 25

--- src/number theory/min25.cpp
+++ src/number theory/min25.cpp
@@ -1,72 +1,106 @@
-#include <bits/stdc++.h>
-#define rep(i, a, b) for (R int i = a; i <= b; i++)
-#define R register
-#define endl putchar('\n')
-#define puts putchar(' ')
-const int maxn = 5000005;
-const long long mod = 1e9 + 7, inv3 = 333333336;
-#define int long long
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, sqr, vis[maxn], pri[maxn], cnt;
-int sp1[maxn], sp2[maxn], w[maxn], tot;
-int id1[maxn], id2[maxn], g1[maxn], g2[maxn];
+using ll=long long;
 
-void get_prime(int n) {
-    vis[1] = 1;
-    rep(i, 2, n) {
-        if (!vis[i]) {
-            pri[++cnt] = i;
-            sp1[cnt] = (sp1[cnt - 1] + i) % mod;
-            sp2[cnt] = (sp2[cnt - 1] + 1ll * i * i) % mod;
-        }
-        for (R int j = 1; j <= cnt && i * pri[j] <= n; j++) {
-            vis[i * pri[j]] = 1;
-            if (!(i % pri[j])) break;
+const ll mod = 1e9+7;
+constexpr ll ksm(ll a,ll b){
+    ll r = 1;
+    while(b){
+        if(b&1)r=r*a%mod;
+        a=a*a%mod;
+        b>>=1;
+    }
+    return r;
+}
+constexpr ll inv2 = ksm(2, mod-2);
+constexpr ll inv6 = ksm(6, mod-2);
+
+constexpr ll sq(ll n){n%=mod;return n*n%mod;}
+constexpr ll sum(ll n){n%=mod;return n*(n+1)%mod*inv2%mod;}
+constexpr ll sum_sq(ll n){n%=mod;return n*(n+1)%mod*(2*n+1)%mod*inv6%mod;}
+
+const int N = 2e5+5;
+
+
+ll p[N],tot; bool vis[N];
+void init_sieve(){
+    for(int i=2;i<N;i++){
+        if(!vis[i])p[++tot]=i;
+        for(int j=1;j<=tot && i*p[j]<N;j++){
+            vis[i*p[j]]=1;
+            if(i%p[j]==0)break;
         }
     }
 }
 
-void get_g() {
-    for (int i = 1; i <= n;) {
-        int j = n / (n / i);
-        w[++tot] = n / i;
-        g1[tot] = w[tot] % mod;
-        g2[tot] = g1[tot] * (g1[tot] + 1) / 2 % mod * (2 * g1[tot] + 1) % mod * inv3 % mod - 1;
-        g1[tot] = g1[tot] * (g1[tot] + 1) / 2 % mod - 1;
-        if (n / i <= sqr)
-            id1[n / i] = tot;
-        else
-            id2[n / (n / i)] = tot;
-        i = j + 1;
+ll v[N],m;
+int id1[N],id2[N];
+ll g1[N],g2[N];
+ll n;
+ll get(ll x){
+    return x<N?id1[x]:id2[n/x];
+}
+void init_sqrt(){
+    for(ll l=1,r;l<=n;l=r+1){
+        r=n/(n/l);
+        v[++m]=n/l;
+        if(v[m]<N)id1[v[m]]=m;
+        else id2[n/v[m]]=m;
+        g1[m]=(sum_sq(v[m])+mod-1)%mod, g2[m]=(sum(v[m])+mod-1)%mod;
     }
-    rep(i, 1, cnt) {
-        for (int j = 1; j <= tot && pri[i] * pri[i] <= w[j]; j++) {
-            int k = w[j] / pri[i] <= sqr ? id1[w[j] / pri[i]] : id2[n / (w[j] / pri[i])];
-            g1[j] = (g1[j] - pri[i] * (g1[k] - sp1[i - 1] + mod)) % mod;
-            g2[j] = (g2[j] - pri[i] * pri[i] * (g2[k] - sp2[i - 1] + mod)) % mod;
+}
+
+void calc_g(){
+    for(int j=1;j<=tot;j++){
+        for(int i=1;i<=m && p[j]<=v[i]/p[j];i++){
+            g1[i] = (g1[i] - sq(p[j])*g1[get(v[i]/p[j])]%mod + sq(p[j])*g1[get(p[j-1])] + mod) % mod;
+            g2[i] = (g2[i] - p[j]*g2[get(v[i]/p[j])]%mod + p[j]*g2[get(p[j-1])] + mod) % mod;
         }
     }
 }
 
-int S(int x, int y) {
-    if (pri[y] >= x) return 0;
-    int k = x <= sqr ? id1[x] : id2[n / x];
-    int res = (g2[k] - g1[k] - (sp2[y] - sp1[y]) + mod) % mod;
-    for (R int i = y + 1; i <= cnt && pri[i] * pri[i] <= x; i++) {
-        int now = pri[i];
-        for (R int e = 1; now <= x; e++, now = now * pri[i]) {
-            res = (res + now % mod * ((now - 1) % mod) % mod * (S(x / now, i) + (e > 1))) % mod;
+ll f(ll x){x%=mod;return (x*x%mod-x+mod)%mod;}
+
+/* 做法一部分 */
+ll S(ll x,int j){
+    if(p[j] >= x) return 0;
+    ll res = (g1[get(x)] - g2[get(x)] - g1[get(p[j])] + g2[get(p[j])]+ mod + mod) % mod;
+    for(int k=j+1;k<=tot && p[k]<=x/p[k];k++){
+        ll w = p[k];
+        for(;w<=x/p[k];w*=p[k]){
+            res = (res + f(w) * S(x/w, k)%mod + f(w*p[k]))%mod;
         }
     }
     return res;
-}
+}  
+/* 做法一部分 */ 
 
-signed main() {
-    scanf("%lld", &n);
-    sqr = sqrt(n);
-    get_prime(sqr);
-    get_g();
-    printf("%lld", (S(n, 0) + 1) % mod);
+/* 做法二部分 */
+ll g[N],S[N];
+/* 做法二部分 */
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    cin>>n;
+    init_sieve();
+    init_sqrt();
+    calc_g();
+
+/* 做法一部分 */ 
+    cout << ((S(n, 0) + 1)%mod+mod)%mod << '\n';
+/* 做法一部分 */ 
+
+/* 做法二部分 */
+    for(int i=1;i<=m;i++)S[i]=g[i]=(g1[i]-g2[i]+mod)%mod;
+    for(int j=tot-1;j>=0;j--){
+        for(int i=1;i<=m && p[j+1]<=v[i]/p[j+1];i++){
+            for(ll w=p[j+1];w<=v[i]/p[j+1];w*=p[j+1]){
+                S[i] = (S[i] + f(w) * (S[get(v[i]/w)] - g[get(p[j+1])] + mod) % mod + f(w*p[j+1]))%mod;
+            }
+        }
+    }
+    cout << S[get(n)]+1 << '\n';
+/* 做法二部分 */
     return 0;
 }
